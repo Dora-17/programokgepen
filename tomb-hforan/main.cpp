f068ab840554e2589ffba05568b7a088546be282
Dora-17 <91243865+Dora-17@users.noreply.github.com>
2021-10-13 09:10:48 +0300

    Add files via upload

---
+++ tomb-hforan/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[5]
+    cout << "n=";
+    cin >> n;
+    srand(time(NULL));
+    int v[5]
+    for(i=0;i<n;i++){
+        cout << "v["<<i<<"]="
+
+} for(int balindex=0,)
+    a=tomb[n-2];
+    b=tomb[2];
+
+    cout <<endl << "szorzatuk = " << a*b<<endl << "osszeguk = " << a+b<<endl;
+
+    int lkkt=a*b;
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        } else{
+            b=b-a;
+        }
+
+    }
+    lkkt/=a;
+    cout << "lkkt=" << lkkt;
+
+
+    cout << endl;
+    cout << "lnko = " << a;
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ veletlentomb1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{int v[100]
+cout << "n=";
+cin >> n;
+for(i=0;i<n;i++){
+    cout << rand()%101 << endl;
+}
+
+int max2,min3,lnko;
+while(max2!=min3){
+    cout << "max2-=min3"{
+    }else {cout << "min3-=max2"
+}
+cout << "lnko"<< lnko;
+return o;
+}
